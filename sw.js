956f3852a3ff0341aa718dbd03a9ceeed79de206
SeptikiSite <septikipro@yandex.ru>
2026-07-05 20:15:44 +0300

    Phase 1.5: analytics, PWA, floating WhatsApp, callback popup

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'septikpro-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/uslugi.html',
+    '/tseny.html',
+    '/o-nas.html',
+    '/kontakty.html',
+    '/manifest.json'
+];
+
+// Install
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch - Network first, fallback to cache
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
